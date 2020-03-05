55ffc5aa26378a826f50cd34fcd1ece5c460ddc7
Carl Ortutay <carlorty@gmail.com>
2020-03-04 20:31:10 -0500

    Prep before db deploy.

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL:
-    process.env.DB_URL ||
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
     "postgresql://ort_cloud:order66@localhost/review-spew",
-  TEST_DB_URL:
-    process.env.TEST_DB_URL ||
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
     "postgresql://ort_cloud:order66@localhost/review-spew-test",
 };
--- test/setup.js
+++ test/setup.js
@@ -4,8 +4,8 @@ const supertest = require("supertest");
 
 process.env.NODE_ENV = "test";
 
-process.env.TEST_DB_URL =
-  process.env.TEST_DB_URL ||
+process.env.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL ||
   "postgresql://ort_cloud:order66@localhost/review-spew-test";
 
 global.expect = expect;
