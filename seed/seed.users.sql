8dc212dc4fa1ab43e8c14f19707c58f1b3a87b38
Carl Ortutay <carlorty@gmail.com>
2020-03-07 11:30:15 -0500

    Added user seed script for development.

---
+++ seed/seed.users.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (username, password) VALUES
+('userone', 'password1'),
+('usertwo', 'password1'),
+('userthree', 'password1'),
+('userfour', 'password1'),
+('userfive', 'password1'),
+('usersix', 'password1'),
+('userseven', 'password1'),
+('usereight', 'password1');
