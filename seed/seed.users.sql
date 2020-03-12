513c0b8d1cd516fa722c06b281fa055e5cb98ada
Carl Ortutay <carlorty@gmail.com>
2020-03-12 14:47:27 -0400

    updated seed

--- seed/seed.users.sql
+++ seed/seed.users.sql
@@ -1,9 +1,10 @@
+BEGIN;
+
+TRUNCATE
+  users
+  RESTART IDENTITY CASCADE;
+
 INSERT INTO users (username, password) VALUES
-('userone', 'password1'),
-('usertwo', 'password1'),
-('userthree', 'password1'),
-('userfour', 'password1'),
-('userfive', 'password1'),
-('usersix', 'password1'),
-('userseven', 'password1'),
-('usereight', 'password1');
+('demo', 'demopass');
+
+COMMIT;
